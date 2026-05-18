40a1068720ad649062cdccdfda70500120cc27f2
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2026-05-18 18:35:50 +0200

    paquete

--- CursoJavaSintaxisBasica/src/temarioPaquetes/Paquetes_I.java
+++ CursoJavaSintaxisBasica/src/temarioPaquetes/Paquetes_I.java
@@ -1,9 +1,28 @@
 package temarioPaquetes;
 
+import java.awt.*; //Esto importa todas las clases del paquete
+import java.util.Scanner;
+
+
+
+
 public class Paquetes_I {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String frase;
+		
+		Button boton;
+		
+		Frame ventana;
+		
+		Color color;
+		
+		Scanner escaner;
+		
+		
+		
 
 	}
 
