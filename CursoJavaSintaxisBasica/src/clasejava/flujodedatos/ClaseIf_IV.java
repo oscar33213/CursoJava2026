d823a412ed7ca35fcd375d45bd0843fe4ff1431c
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2026-05-21 18:16:29 +0200

    fixed

--- CursoJavaSintaxisBasica/src/clasejava/flujodedatos/ClaseIf_IV.java
+++ CursoJavaSintaxisBasica/src/clasejava/flujodedatos/ClaseIf_IV.java
@@ -1,55 +1,42 @@
 package clasejava.flujodedatos;
+
 import javax.swing.*;
+
 public class ClaseIf_IV {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		// IF ANIDADO
-		
-		int edad = Integer.parseInt(JOptionPane.showInputDialog("Indica tu edad: "));
-		
-		
-		
-		try {
-			
-			if (edad < 0) {
-				throw new IllegalArgumentException ("La edad no puede ser menor a 0");
-				
-			}
-			
-			
-			
-			if (edad > 18) {
-				
-				String carnet = JOptionPane.showInputDialog("¿Tienes carnet de conducir? (S/N)");
-				
-				if (!carnet.equalsIgnoreCase("S") && !carnet.equalsIgnoreCase("N")) {
-					
-					throw new IllegalArgumentException ("Solo S o N");
-					
-				}
-				
-					if (carnet.equalsIgnoreCase("S")) {
-						
-						JOptionPane.showMessageDialog(null, "Puedes comprarte el coche");
-				}
-					} else { 
-				
-				JOptionPane.showMessageDialog(null, "No puedes comprarte el coche");
-			}
-			
-		} catch (Exception e) {
-			
-			System.out.println("Error: " + e.getMessage());
-			return;
-		}
-		
-		
-		
-		
+    public static void main(String[] args) {
 
-	}
+        // IF ANIDADO
 
+        int edad;
+
+        try {
+            edad = Integer.parseInt(JOptionPane.showInputDialog("Indica tu edad: "));
+
+            if (edad < 0) {
+                throw new IllegalArgumentException("La edad no puede ser menor a 0");
+            }
+
+            if (edad > 18) {
+
+                String carnet = JOptionPane.showInputDialog("¿Tienes carnet de conducir? (S/N)");
+
+                if (!carnet.equalsIgnoreCase("S") && !carnet.equalsIgnoreCase("N")) {
+                    throw new IllegalArgumentException("Solo S o N");
+                }
+
+                if (carnet.equalsIgnoreCase("S")) {
+                    JOptionPane.showMessageDialog(null, "Puedes comprarte el coche");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No puedes comprarte el coche");
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No puedes comprarte el coche");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
